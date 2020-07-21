d8b413d086ea9f467b97ac6099d37521897f442a
Josh Grams <josh@qualdan.com>
2020-07-21 05:38:38 -0400

    Gutenberg: better titles.

--- gutenberg.js
+++ gutenberg.js
@@ -1,11 +1,4 @@
-/**
- * Generate an exercise based on gutenberg-data.js (make sure to import that)
- * 
- * @param {number} word_count The exercise will contain this many words
- * @param {number} top_n Sentences will contain only the top N number of words (clamped between 100 and 8000)
- * @param {function} rng A seed for the RNG
- */
-function generateExercise(word_count, top_n, rng) {
+function clamp_top_n(top_n) {
     // round up
     top_n = Math.floor((top_n + 99) / 100) * 100
     if (top_n < 100) {
@@ -14,7 +7,19 @@ function generateExercise(word_count, top_n, rng) {
     if (top_n > 8000) {
         top_n = 8000
     }
+    return top_n
+}
 
+
+/**
+ * Generate an exercise based on gutenberg-data.js (make sure to import that)
+ *
+ * @param {number} word_count The exercise will contain this many words
+ * @param {number} top_n Sentences will contain only the top N number of words (clamped between 100 and 8000)
+ * @param {function} rng A seed for the RNG
+ */
+function generateExercise(word_count, top_n, rng) {
+    top_n = clamp_top_n(top_n)
     let words = [], sentence
     let chars_left = word_count * 5
     let top_n_bucket = window.sentences_by_top_100[top_n / 100 - 1]
@@ -33,9 +38,11 @@ window.onload = function () {
     var rng = new_rng(fields.seed)
 
     var top_n = fields.top == null ? 100 : parseInt(fields.top)
+    top_n = clamp_top_n(top_n)
     var word_count = fields.word_count == null ? 100 : parseInt(fields.word_count)
 
-    var name = "Sentences from Project Gutenberg that contain only the top " + top_n + " words.";
+    var from_n = top_n - 99, to_n = top_n
+    var name = "Project Gutenberg sentences for words " + from_n + " to " + to_n + ".";
     var hints = initializeHints(fields.hints, fields.floating_hints);
     var exercise = generateExercise(word_count, top_n, rng);
 
