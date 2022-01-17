JoshuaGrams/steno-jig

commit ae98efabf42ed2586269bf89d529386dee358d01

Alternate a fixed word or phrase with drill words.
Speedbuilding quick tip #4 from https://www.chicorymeadow.com/wp-content/uploads/Speedbuilding-Tips-by-Carol-Jochim.pdf

type-jig.js
@@ -5,7 +5,7 @@
  * `output`, and `clock` are elements (or element ID strings).
  */
 
-function TypeJig(exercise, display, results, input, clock, hint, speed) {
+function TypeJig(exercise, display, results, input, clock, hint, options) {
 	this.exercise = exercise;
 	this.display = documentElement(display);
 	this.input = documentElement(input);
@@ -17,11 +17,15 @@ function TypeJig(exercise, display, results, input, clock, hint, speed) {
 
 	this.lookahead = 1000;
 
-	if(speed) {
-		if(speed.wpm !== '' && Math.floor(+speed.wpm) == speed.wpm) {
-			this.speed = {type: 'wpm', value: speed.wpm}
-		} else if(speed.cpm !== '' && Math.floor(+speed.cpm) == speed.cpm) {
-			this.speed = {type: 'cpm', value: speed.cpm}
+	if(options) {
+		if(options.wpm !== '' && Math.floor(+options.wpm) == options.wpm) {
+			this.options = {type: 'wpm', value: options.wpm}
+		} else if(options.cpm !== '' && Math.floor(+options.cpm) == options.cpm) {
+			this.options = {type: 'cpm', value: options.cpm}
+		}
+		if(typeof options.alternate === 'string' && options.alternate !== '') {
+			this.alternateWith = TypeJig.wordsAndSpaces(options.alternate)
+			this.alternateWith.push(' ')
 		}
 	}
 
@@ -278,6 +282,19 @@ TypeJig.prototype.getWords = function(n) {
 		var text = this.exercise.getText();
 		if(text) {
 			var pieces = TypeJig.wordsAndSpaces(text);
+			if(this.alternateWith) {
+				let alt = this.alternateWith
+				var words = []
+				for(let i=0; i<pieces.length; ++i) {
+					if(/^\S+$/.test(pieces[i])) {
+						for(let j=0; j<alt.length; ++j) {
+							words.push(alt[j])
+						}
+					}
+					words.push(pieces[i])
+				}
+				pieces = words
+			}
 			for(let i=0; i<pieces.length; ++i) {
 				var span = document.createElement('span');
 				span.appendChild(document.createTextNode(pieces[i]));

form.html
@@ -28,6 +28,10 @@
 		<div name="speed">
 			<label><input id="wpm" type="number" name="wpm" value="" step="5"> Speed (words per minute)
 				(or <input type="checkbox" id="cpm" value="1"> x5 chars per minute) </label>
+		</div><br>
+
+		<div name="alt">
+			<label>Alternate with: <input id="alternate" type="text" name="alternate"></label>
 		</div>
 	</div>
 
@@ -39,6 +43,7 @@
 			<input type="hidden" name="wpm" value="">
 			<input type="hidden" name="cpm" value="">
 			<input type="hidden" name="hints" value="">
+			<input type="hidden" name="alternate" value="">
 
 			<textarea name="drill"></textarea><br>
 			<input type="checkbox" name="type" value="shuffled"> Shuffle words.<br>
@@ -73,6 +78,7 @@
 			<input type="hidden" name="wpm" value="">
 			<input type="hidden" name="cpm" value="">
 			<input type="hidden" name="hints" value="">
+			<input type="hidden" name="alternate" value="">
 
 			Drill
 			<input type="number" name="count" value="100" min="25" max="1000" step="25">
@@ -94,6 +100,7 @@
 			<input type="hidden" name="wpm" value="">
 			<input type="hidden" name="cpm" value="">
 			<input type="hidden" name="hints" value="">
+			<input type="hidden" name="alternate" value="">
 
 			Drill sentences containing only the top
 			<input type="number" name="top" value="100" min="100" max="8000" step="100">
@@ -118,6 +125,7 @@
 			<input type="hidden" name="wpm" value="">
 			<input type="hidden" name="cpm" value="">
 			<input type="hidden" name="hints" value="">
+			<input type="hidden" name="alternate" value="">
 
 			Drill Length:
 			<input type="number" name="word_count" value="100" min="50" max="1000" step="50">
@@ -137,6 +145,7 @@
 			<input type="hidden" name="wpm" value = "">
 			<input type="hidden" name="cpm" value="">
 			<input type="hidden" name="hints" value="">
+			<input type="hidden" name="alternate" value="">
 
 			Drill length:
 			<ul>
@@ -159,6 +168,7 @@
 			<input type="hidden" name="wpm" value="">
 			<input type="hidden" name="cpm" value="">
 			<input type="hidden" name="hints" value="">
+			<input type="hidden" name="alternate" value="">
 
 			Drill Time:
 			<input type="number" name="timeLimit" value="3" min="1" max="60" size="3">
@@ -178,6 +188,7 @@
 			<input type="hidden" name="wpm" value="">
 			<input type="hidden" name="cpm" value="">
 			<input type="hidden" name="hints" value="">
+			<input type="hidden" name="alternate" value="">
 
 			Drill Time:
 			<input type="number" name="timeLimit" value="3" min="1" max="60">
@@ -257,10 +268,10 @@ function runExercise(fields) {
 			var translations = TypeJig.shortestTranslations(TypeJig.Translations.Plover);
 			var hints = new StenoDisplay(strokes, translations, true);
 		}
-		var speed = {wpm: fields.wpm, cpm: fields.cpm};
+		var options = {wpm: fields.wpm, cpm: fields.cpm, alternate: fields.alternate};
 
 		displayOnly('lesson');
-		return setExercise(exercise.name, exercise, hints, speed);
+		return setExercise(exercise.name, exercise, hints, options);
 	}
 }
 
@@ -268,9 +279,11 @@ function getSettings() {
 	var wpm = document.getElementById('wpm');
 	var cpm = document.getElementById('cpm');
 	var hints = document.getElementById('hints');
+	var alt = document.getElementById('alternate');
 	if(cpm.checked) this.elements.cpm.value = 5 * wpm.value;
 	else this.elements.wpm.value = wpm.value;
 	this.elements.hints.value = hints.checked ? hints.value : '';
+	if(this.elements.alternate) this.elements.alternate.value = alt.value;
 }
 
 function runCustom(evt) {
@@ -281,7 +294,9 @@ function runCustom(evt) {
 		localStorage.custom = fields.drill
 	}
 	if(fields.shuffleLines) {
+		// Don't shuffle words.
 		delete(fields.type);
+		// Pre-shuffle lines.
 		fields.drill = shuffle(fields.drill.trim().split(/\n+/m)).join(' ');
 	}
 	TypeJig.WordSets.custom = fields.drill.trim().split(/\s+/m);

gutenberg.js
@@ -44,7 +44,7 @@ window.onload = function () {
     var from_n = top_n - 99, to_n = top_n
     var name = "Project Gutenberg sentences for words " + from_n + " to " + to_n + ".";
     var hints = initializeHints(fields.hints, fields.floating_hints);
-	var speed = {wpm: fields.wpm, cpm: fields.cpm};
+	var speed = {wpm: fields.wpm, cpm: fields.cpm, alternate: fields.alternate};
 
     var exercise = generateExercise(word_count, top_n, rng);
 

markov.js
@@ -47,7 +47,7 @@ window.onload = function() {
 
 	var name = "Markov-chain generated sentences";
 	var hints = initializeHints(fields.hints, fields.floating_hints);
-	var speed = {wpm: fields.wpm, cpm: fields.cpm};
+	var speed = {wpm: fields.wpm, cpm: fields.cpm, alternate: fields.alternate};
 
 	const ngrams = compute_ngrams(sentences, 3);
 	var exercise = generateMarkovExercise(ngrams, word_count, rng);

monkeytype-quote.js
@@ -39,7 +39,7 @@ let jig, exercise, fields
 function go(ex) {
 	fields = parseQueryString(document.location.search)
 	let hints = initializeHints(fields.hints, fields.floating_hints)
-	let speed = {wpm: fields.wpm, cpm: fields.cpm}
+	let speed = {wpm: fields.wpm, cpm: fields.cpm, alternate: fields.alternate}
 
 	if(ex == null) {
 		switch(fields.length) {

util.js
@@ -170,6 +170,15 @@ function loadSettings() {
 			localStorage.wpm = wpm.value
 		})
 	}
+
+	// ALTERNATE
+	const alt = document.getElementById('alternate')
+	if(alt && alt.nodeName === 'INPUT' && alt.type === 'text') {
+		if(localStorage.alternate != null) alt.value = localStorage.alternate
+		alt.addEventListener('input', function(evt) {
+			localStorage.alternate = alt.value
+		})
+	}
 }
 
 /**

commit 86597e735d2096a12045406cfca1054eb8581d31

Oops, fix speed (broke with ae98efa alternation).

type-jig.js
@@ -19,9 +19,9 @@ function TypeJig(exercise, display, results, input, clock, hint, options) {
 
 	if(options) {
 		if(options.wpm !== '' && Math.floor(+options.wpm) == options.wpm) {
-			this.options = {type: 'wpm', value: options.wpm}
+			this.speed = {type: 'wpm', value: options.wpm}
 		} else if(options.cpm !== '' && Math.floor(+options.cpm) == options.cpm) {
-			this.options = {type: 'cpm', value: options.cpm}
+			this.speed = {type: 'cpm', value: options.cpm}
 		}
 		if(typeof options.alternate === 'string' && options.alternate !== '') {
 			this.alternateWith = TypeJig.wordsAndSpaces(options.alternate)